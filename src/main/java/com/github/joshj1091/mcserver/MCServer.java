da21127538b63ad065df7d949e8d386c4cba42c2
Josh <joshjames1091@gmail.com>
2016-04-20 13:28:39 +0100

    Just use System.out for debug (supid Logger formatting)

--- src/main/java/com/github/joshj1091/mcserver/MCServer.java
+++ src/main/java/com/github/joshj1091/mcserver/MCServer.java
@@ -1,4 +1,16 @@
 package com.github.joshj1091.mcserver;
 
 public class MCServer {
+
+    public MCServer() {
+        log("MinecraftServer has started");
+    }
+
+    public void log(String message) {
+        System.out.println("INFO: " + message);
+    }
+
+    public void error(String message) {
+        System.out.println("ERROR: " + message);
+    }
 }
--- src/main/java/com/github/joshj1091/mcserver/bootstrap/Bootstrap.java
+++ src/main/java/com/github/joshj1091/mcserver/bootstrap/Bootstrap.java
@@ -2,9 +2,19 @@ package com.github.joshj1091.mcserver.bootstrap;
 
 import com.github.joshj1091.mcserver.MCServer;
 
+import java.util.Scanner;
+
 public class Bootstrap {
 
     public static void main(String[] args) {
         MCServer server = new MCServer();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+
+            server.log(input);
+        }
     }
 }
