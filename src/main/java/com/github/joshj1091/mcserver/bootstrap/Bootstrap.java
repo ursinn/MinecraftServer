019edb41b39ecb89ad366a1f71d559b2949292a4
Josh <joshjames1091@gmail.com>
2016-04-20 13:16:54 +0100

    Add bootstrap

---
+++ src/main/java/com/github/joshj1091/mcserver/bootstrap/Bootstrap.java
@@ -0,0 +1,10 @@
+package com.github.joshj1091.mcserver.bootstrap;
+
+import com.github.joshj1091.mcserver.MCServer;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        MCServer server = new MCServer();
+    }
+}
